243833fe113b2bc740948cfec95043e7b7dc9e96
Robin Hafid Quintero Lopez <rohaquinlop301@gmail.com>
2020-11-18 23:15:28 -0500

    Entrega 1
    Accepted

--- capital.py
+++ capital.py
@@ -11,10 +11,35 @@ MOD = 7340033
 factorial = []
 inverse = []
 
+## Initializing factorial and inverse arrays
+
+"""
+This procedure precalculate the factorial for each value in the range of 0 to 10^5.
+Complexity O(n).
+"""
+factorial = [1 for _ in range(100001)]
+
+for i in range(1, 100001):
+  factorial[i] = (factorial[i-1]*(i%MOD))%MOD
+
+"""
+This procedure precalculate the modular inverse modulo MOD for the values in the range of 0 and MOD-1.
+This procedure is correcto due to:
+the inverse[i] = ((MOD-(MOD//i))*inverse[MOD%i])%MOD. Only apply if i >= 2, if i = 1 then the inverse is equal to 1.
+Complexity O(n).
+"""
+
+inverse = [0 for _ in range(100001)]
+inverse[1] = 1
+
+for i in range(2, 100001):
+  inverse[i] = ((MOD-(MOD//i))*inverse[MOD%i])%MOD
+
 
 def getQuadrant(x, y):
   """
-  This functions have 2 parameters, x and y (-10^9 <= x, y <= 10^9) and returns the quadrant where the (x, y) is located.
+  Given and x and y, this function returns the quadrant where this coordinates points to.
+  The complexity is O(1).
   """
   if x > 0 and y > 0:
     return 1
@@ -26,48 +51,83 @@ def getQuadrant(x, y):
     return 4
 
 def C(n, k):
+  """
+  Given an n and k, this function returns de Combinatorics of n between k.
+  To make this optimal two arrays are used (factorial and inverse).
+  factorial contains precalculated the factorial values in the range of 0 and 10^5 for each value.
+  inverse contains precalculated the modular inverse modulo MOD for values in the range of 0 and MOD-1.
+
+  The combinatorics equation is iqual to: n!/(k!*(n-k)!).
+  But this also can be seen as: n! * ( k!*(n-k)! )^-1.
+
+  Due that the result must be given modulo MOD then can be used the precalculated inverse.
+  The complexity is O(1).
+  """
   global factorial, inverse, MOD
-  ans = None
+  res = None
   if k == 0 or k == n:
-    ans = 1
+    res = 1
   else:
-    ans = (factorial[n] * inverse[ (factorial[k]*factorial[n-k])%MOD ]%MOD)%MOD
-  return ans
+    res = (factorial[n] * inverse[ (factorial[k]*factorial[n-k])%MOD ]%MOD)%MOD
+  return res
 
-## Initializing factorial and inverse arrays
-factorial = [1 for _ in range(100001)]
-
-for i in range(1, 100001):
-  factorial[i] = (factorial[i-1]*(i%MOD))%MOD
-
-inverse = [0 for _ in range(MOD)]
-inverse[1] = 1
-
-for i in range(2, MOD):
-  inverse[i] = ((MOD-(MOD//i))*inverse[MOD%i])%MOD
-
-def buildPolynomial(x, y):
+def polynomial(x, y):
+  """
+  Given an x and y, this function returns an array which contains the coefficients of the polynomial that's being computed.
+  Each value for the polynomial is computed in the way that res[i] = C(x, i)*C(y, i).
+  Return the res array.
+  
+  Complexity O(n).
+  """
   k = min(x, y)
+  res = []
 
-  ans = []
+  i = 0
+  while i <= k:
+    res.append( (C(x, i)*C(y, i))%MOD )
+    i += 1
 
-  for i in range(k+1):
-    val = C(x, i)*C(y, i)
-    ans.append(val)
-  return ans
+  return res
 
-def solve(quadrants):
+def FFTBruteForce(A, B):
+  """
+  Given 2 arrays A, B returns the multiplication of both arrays (Polynomial).
+  """
+  res = [0 for _ in range(len(A)+len(B)-1)]
+
+  for i in range(len(A)):
+    for j in range(len(B)):
+      res[i+j] += ((A[i]%MOD)*(B[j]%MOD))%MOD
+  
+  return res
+
+def solve(quadrants, n):
+  """
+  Given a quadrants array, gives the answer.
+  """
   global factorial, inverse, MOD
+  out = ""
 
-  P1 = buildPolynomial(quadrants[0], quadrants[2])
-  P2 = buildPolynomial(quadrants[1], quadrants[3])
+  res = FFTBruteForce(polynomial(quadrants[0], quadrants[2]), polynomial(quadrants[1], quadrants[3]))
+  j = 1
 
-  print(P1, P2)
+  for i in range(n):
+    if (i+1)%2 != 0 or ((i+1)%2 == 0 and j >= len(res)):
+      out += "0"
+    else:
+      out += "{}".format(res[j])
+      j += 1
+    
+    if i < n-1:
+      out += " "
+  out += "\n"
+  return out
 
 def main():
-  global MOD, factorial, inverse
   lines = stdin.readlines()
   i = 1
+  out = ""
+  cases = 1
 
   while i < len(lines):
     n = int(lines[i].strip())
@@ -81,6 +141,9 @@ def main():
       quadrants[getQuadrant(x, y)-1] += 1
       i += 1
     
-    solve(quadrants)
+    out += "Case {}:\n".format(cases)
+    out += solve(quadrants, n)
+    cases += 1
+  print(out.strip())
 
 main()
